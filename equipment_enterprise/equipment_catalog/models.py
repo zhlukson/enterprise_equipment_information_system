a7eefa2ad482a4ac9697c9b4f4ae6117b3731fc1
unknown <kloch3737@gmail.com>
2023-01-22 17:11:13 +0300

    changing the structure of the database (add Production, Workshops,  EquipmentLocation models)

--- equipment_enterprise/equipment_catalog/models.py
+++ equipment_enterprise/equipment_catalog/models.py
@@ -62,7 +62,6 @@ class Equipment(models.Model):
     information_employee = models.FileField(upload_to='description_equipment/%Y/%m/%d/', verbose_name='Характеристики')
     additional_information = models.URLField(verbose_name='Дополнительная информация', null=True)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d', verbose_name='Фото')
-    location = models.CharField(max_length=255, verbose_name='Расположение')
 
     def __str__(self):
         return f'{self.brand} {self.model}'
@@ -91,3 +90,40 @@ class EquipmentEmployee(models.Model):
 
     def __str__(self):
         return f'{self.equipment} - {self.employee}'
+
+
+class Production(models.Model):
+    class Meta:
+        verbose_name = 'Производство'
+        verbose_name_plural = 'Производства'
+
+    production_name = models.CharField(max_length=100, verbose_name='Название')
+
+    def __str__(self):
+        return self.production_name
+
+class Workshops(models.Model):
+    class Meta:
+        verbose_name = 'Цех'
+        verbose_name_plural = 'Цеха'
+
+    number = models.IntegerField(verbose_name='Номер цеха', primary_key=True)
+    production = models.ForeignKey('Production', on_delete=models.PROTECT, verbose_name='Производство')
+
+    def __str__(self):
+        return f'{self.production} производство, {self.number} цех'
+
+
+class EquipmentLocation(models.Model):
+    class Meta:
+        verbose_name = 'Оборудование - Расположение'
+        verbose_name_plural = 'Оборудование - Расположение'
+        unique_together = ['location', 'equipment']
+
+    location = models.ForeignKey('Workshops', on_delete=models.PROTECT, verbose_name='Расположение')
+    equipment = models.ForeignKey('Equipment', on_delete=models.PROTECT, verbose_name='Оборудование')
+
+    def __str__(self):
+        return f'{self.equipment} --> {self.location}'
+
+
